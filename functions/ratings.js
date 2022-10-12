ce4220ff4870955d050b00ed80281ea0c7451a15
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-10-12 12:04:56 -0500

    stylefix

--- functions/ratings.js
+++ functions/ratings.js
@@ -34,10 +34,8 @@ function build(data,values){
         }
        
         let parent = `
-        <div class="container">
         <div class="smileybox">
         ${output}
-        </div>
         </div>
         `;
         let obj = document.getElementById('rating');
@@ -47,14 +45,12 @@ function build(data,values){
     else{
        let obj = document.getElementById('rating');
        obj.innerHTML = `
-       <div class="container">
         <div class="smileybox">
             <label for="r1" class="check"><input type="checkbox" value="${value}" id="r1" onchange="ratingStar(event)"/><i class="em em-weary"></i></label>
             <label for="r2" class="check"><input type="checkbox" value="${value}"  id="r2" onchange="ratingStar(event)"/><i class="em em-worried"></i></label>
             <label for="r3" class="check"><input type="checkbox" value="${value}"  id="r3" onchange="ratingStar(event)"/><i class="em em-blush"></i></label>
             <label for="r4" class="check"><input type="checkbox" value="${value}"  id="r4" onchange="ratingStar(event)"/><i class="em em-smiley"></i></label>
             <label for="r5" class="check"><input type="checkbox" value="${value}"  id="r5" onchange="ratingStar(event)"/><i class="em em-sunglasses"></i></label>
-        </div>
         </div>
        `;
     }
